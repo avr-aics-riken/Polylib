96d571bb8d2cf26e7a0a330a368a17470ed96d33
Kenji Ono <keno@riken.jp>
2013-09-26 18:21:53 +0900

    r32 Revert "r29 update PolygonGroup::set_all_exid_of_trias() [ver 2.6.8]"
    This reverts commit ef381e931dea4875dc2be0e10cd033a5bcadd27f.

--- src/groups/PolygonGroup.cxx
+++ src/groups/PolygonGroup.cxx
@@ -583,8 +583,6 @@ POLYLIB_STAT PolygonGroup::rescale_polygons( float scale )
 // public /////////////////////////////////////////////////////////////////////
 POLYLIB_STAT PolygonGroup::set_all_exid_of_trias( int id )
 {
-  m_id = id;           // keno 2013-07-20
-  m_id_defined = true; // keno 2013-07-20
 	return m_polygons->set_all_exid( id );
 }
 
