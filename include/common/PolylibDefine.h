f94cc3e6f82f7e58c2c11955dbf038bf925e0891
fuchikamisatoshi <fuchikami@jp.fujitsu.com>
2014-02-07 14:37:05 +0900

    float , double switch option

--- include/common/PolylibDefine.h
+++ include/common/PolylibDefine.h
@@ -12,19 +12,10 @@
 #ifndef polylib_define_h
 #define polylib_define_h
 
-#include <float.h>
 
 
-//PL_REALはgcc -Dで定義済み   -Dの中に定義されている値は見えないので,configure等に例REAL_TYPE_FLOATなど  float/doubleを分けるヒントになる別のdefineを用意する必要がある
-//#ifdef PL_REAL
-//#define REAL_TYPE float
 #define REAL_TYPE PL_REAL
-#define REAL_VAL_MAX FLT_MAX;
-/*
-#else	
-#define REAL_TYPE double
-#define REAL_VAL_MAX DBL_MAX;
-#endif
-*/
+
+
 
 #endif // polylib_define_h
--- src/polygons/VertexList.cxx
+++ src/polygons/VertexList.cxx
@@ -17,6 +17,7 @@
 #include <map>
 #include <iostream>
 #include <string>
+#include <float.h>
 
 #include "common/Vec3.h"
 #include "common/BBox.h"
@@ -281,7 +282,7 @@ namespace PolylibNS{
 
 		// MOD S fuchi warning C4756: 定数演算でオーバーフローを起こしました。
 		//REAL_TYPE min_distance=1.0e107;
-		REAL_TYPE min_distance= REAL_VAL_MAX;
+		REAL_TYPE min_distance= (sizeof(REAL_TYPE)==4)?FLT_MAX:DBL_MAX ;
 		// MOD E 
 		int index=0;
 		int isave=-1;
